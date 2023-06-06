2f23e39bea7df5d29e8f075a74de174e68ffbf78
paulinakaras <ppaulina.karas@gmail.com>
2023-06-06 20:54:53 +0200

    refactored second test case

--- cypress/constans/puppyAdditionalProducts.constans.js
+++ cypress/constans/puppyAdditionalProducts.constans.js
@@ -1,6 +1,7 @@
 export const PUPPY_ADDITIONAL_PRODUCTS = {
-    CHEW_TOY: '#toy',
-    TRAVEL_CARRIER: '#carrier',
+    COLLAR_AND_LEASH_CHECKBOX: '#collar',
+    CHEW_TOY_CHECKBOX: '#toy',
+    TRAVEL_CARRIER_CHECKBOX: '#carrier',
 
     COMPLETE_THE_ADOPTION_BUTTON: '[action="/orders/new"]'
 };
--- cypress/constans/puppyList.constans.js
+++ cypress/constans/puppyList.constans.js
@@ -1,5 +1,8 @@
 export const PUPPY_LIST = {
     BROOK_VIEW_DETAILS_BUTTON: '[action="/puppies/4"]',
+    SPARKY_VIEW_DETAILS_BUTTON: '[action="/puppies/9"]',
+
+    NEXT_PAGE_BUTTON: '.next_page',
 
     ADOPTION_MESSAGE: '#notice'
 };
